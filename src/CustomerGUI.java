d56d31f3c03ecbb2a235ea45591c2cf97b52d561
questionyu <questionyu@outlook.com>
2017-04-03 16:13:12 +0800

    Implements ActionListener and move Kiosk into it.

--- src/CustomerGUI.java
+++ src/CustomerGUI.java
@@ -4,18 +4,17 @@
  */
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Title        CustomerGUI.java
  * Description
  */
 
-public class CustomerGUI {
-	/**
-	 * This method create a JFrame and set some settings.
-	 */
-	private CustomerGUI() {
-
+public class CustomerGUI implements ActionListener {
+	private static void createAndShowGUI() {
 		// Set up the frame and display it.
 		JFrame frame = new JFrame("Self-service Ticketing Kiosk");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -23,7 +22,8 @@ public class CustomerGUI {
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 
-		frame.setContentPane(new Kiosk());
+		CustomerGUI demo = new CustomerGUI();
+		frame.setContentPane(demo.createCinemaKiosk());
 		frame.setVisible(true);
 	}
 
@@ -31,6 +31,37 @@ public class CustomerGUI {
 	 * Program starts here.
 	 */
 	public static void main(String[] args) {
-		SwingUtilities.invokeLater(CustomerGUI::new);
+		SwingUtilities.invokeLater(CustomerGUI::createAndShowGUI);
+	}
+
+	/**
+	 * This method create a JPanel and set some settings.
+	 */
+	private JPanel createCinemaKiosk() {
+		JPanel totalGUI = new JPanel();
+		totalGUI.setLayout(new BorderLayout());
+		totalGUI.setBackground(new Color(90, 154, 212));
+		JLabel helloLabel = new JLabel("Welcome!", JLabel.CENTER);
+		helloLabel.setFont(new Font("Helvetica", Font.BOLD, 50));
+		helloLabel.setForeground(Color.BLACK);
+
+		JButton enter = new JButton("Enter");
+		enter.setFont(new Font("Helvetica", Font.BOLD, 30));
+		enter.setForeground(Color.BLACK);
+		enter.addActionListener(e -> {
+		});
+		JPanel helloSouthPanel = new JPanel();
+		helloSouthPanel.setOpaque(false);
+		helloSouthPanel.add(enter);
+
+		totalGUI.add(helloLabel, BorderLayout.CENTER);
+		totalGUI.add(helloSouthPanel, BorderLayout.SOUTH);
+
+		return totalGUI;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
 	}
 }

49024d3e40436e8c8c1f6350d13f735265b18da9
questionyu <questionyu@outlook.com>
2017-04-18 18:37:38 +0800

    Main GUI uses CardLayout.

--- src/KioskInterface.java
+++ src/KioskInterface.java
@@ -1,13 +1,10 @@
-/*
- * Created by Question on 4/4/2017
- * Copyright (c) 2017. All Rights Reserved.
- */
-
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 
+import javax.swing.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -16,10 +13,45 @@ import java.util.ArrayList;
  * Description  This class contains the kiosk interface's definition.
  */
 class KioskInterface {
-	private File filmXMLFile;
+	private File filmXMLFile = new File("films.xml");
 
-	KioskInterface() {
-		filmXMLFile = new File("films.xml");
+	private JFrame kioskFrame;
+	private CardLayout kioskCardLayout;
+	private JPanel kioskPanel;
+	private Color backgroundColor = new Color(90, 154, 212);
+
+	private KioskInterface() {
+		kioskFrame = new JFrame("Self-service Ticketing Kiosk");
+		kioskFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		kioskFrame.setSize(960, 540);
+		kioskFrame.setResizable(false);
+		kioskFrame.setLocationRelativeTo(null);
+
+		kioskCardLayout = new CardLayout();
+		kioskPanel = new JPanel(kioskCardLayout);
+		kioskPanel.setBackground(backgroundColor);
+		kioskFrame.add(kioskPanel);
+
+		kioskFrame.setVisible(true);
+	}
+
+	/**
+	 * Program starts here.
+	 *
+	 * @param args The input parameters.
+	 */
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> (new KioskInterface()).welcome());
+	}
+
+	private void welcome() {
+		kioskPanel.add(new WelcomeScreen(this), "WelcomeScreen");
+		kioskCardLayout.show(kioskPanel, "WelcomeScreen");
+	}
+
+	void listFilm() {
+		kioskPanel.add(new ListFilmScreen(this), "ListFilmScreen");
+		kioskCardLayout.show(kioskPanel, "ListFilmScreen");
 	}
 
 	ArrayList<Film> getFilm() {

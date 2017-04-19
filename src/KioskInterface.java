3998dd8e9c70cb4b13c55d63f7878eb788cbf767
cshinoc <623798816@qq.com>
2017-04-19 19:08:07 +0800

    Remove test var.

--- src/KioskInterface.java
+++ src/KioskInterface.java
@@ -19,7 +19,6 @@ class KioskInterface {
 	private CardLayout kioskCardLayout;
 	private JPanel kioskPanel;
 	private Color backgroundColor = new Color(90, 154, 212);
-	private int testint;
 
 	private KioskInterface() {
 		kioskFrame = new JFrame("Self-service Ticketing Kiosk");

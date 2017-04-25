2aabe4f7c2bcb17b8819f4260401e28bf3b663de
questionyu <questionyu@outlook.com>
2017-04-25 23:04:46 +0800

    Add many variables to Film class and related getter and setter functions.

--- src/Film.java
+++ src/Film.java
@@ -1,10 +1,35 @@
+import java.util.ArrayList;
+
 /**
  * Title        Film.java
  * Description  This class store the information of movie.
  */
 class Film {
 	private String name;
-	private int num;
-	private int time;
+	private int length;
+	private double price;
+	private ArrayList<String> screenings;
 
+	Film(String name, int length, double price, ArrayList<String> screenings) {
+		this.name = name;
+		this.length = length;
+		this.price = price;
+		this.screenings = screenings;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	int getLength() {
+		return length;
+	}
+
+	double getPrice() {
+		return price;
+	}
+
+	ArrayList<String> getScreenings() {
+		return screenings;
+	}
 }
